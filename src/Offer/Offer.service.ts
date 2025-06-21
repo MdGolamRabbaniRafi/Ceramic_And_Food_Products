6210e2ed8938abf25d96a17a4cedf7956249bc49
MdGolamRabbaniRafi <dreamlessboyrafi7@gmail.com>
2025-06-21 18:20:29 +0600

    Offer crud5

--- src/Offer/Offer.service.ts
+++ src/Offer/Offer.service.ts
@@ -105,25 +105,26 @@ export class OfferService {
     const offer = await this.getOfferById(id);
     return { message: `checking: ${JSON.stringify(offer)}`, success: false };
   }
+  //delete
 
-    // if ('message' in offer) {
-    //   return { message: offer.message, success: false }
-    // }
+  // if ('message' in offer) {
+  //   return { message: offer.message, success: false }
+  // }
 
-    // // Check if image exists and delete it
-    // if (offer.image) {
-    //   const check = await this.deleteImageFile(offer.image);
-    //   if (check.message != "File deleted successfully") {
-    //     return { message: check.message, success: false }
-    //   }
-    // }
+  // // Check if image exists and delete it
+  // if (offer.image) {
+  //   const check = await this.deleteImageFile(offer.image);
+  //   if (check.message != "File deleted successfully") {
+  //     return { message: check.message, success: false }
+  //   }
+  // }
 
-    // try {
-    //   await this.offerRepository.delete(id);
-    //   return { message: `Offer with ID ${id} deleted successfully.`, success: true };
-    // } catch (error) {
-    //   return { message: 'Error removing offer', success: false };
-    // }
+  // try {
+  //   await this.offerRepository.delete(id);
+  //   return { message: `Offer with ID ${id} deleted successfully.`, success: true };
+  // } catch (error) {
+  //   return { message: 'Error removing offer', success: false };
+  // }
   // }
 
 
