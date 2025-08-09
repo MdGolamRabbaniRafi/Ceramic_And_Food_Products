439a87a385eda03aeced2bcf658b309c802fed57
MdGolamRabbaniRafi <dreamlessboyrafi7@gmail.com>
2025-08-09 22:58:35 +0600

    debug3

--- dist/Payment/Payment.service.js
+++ dist/Payment/Payment.service.js
@@ -44,10 +44,10 @@ let PaymentService = class PaymentService {
         const userEmail = paymentEntity.user?.email;
         const adminEmail = this.configService.get('ADMIN_EMAIL');
         if (!userEmail) {
-            throw new Error(`User email is missing. user:${paymentEntity}`);
+            throw new Error(`User email is missing.`);
         }
         if (!adminEmail) {
-            throw new Error('Admin email is missing');
+            throw new Error(`Admin email is missing.${adminEmail}`);
         }
         const adminNumber = process.env.Admin_Number;
         const message = `
